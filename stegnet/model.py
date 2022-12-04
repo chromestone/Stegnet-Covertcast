e547ebc7c87de5c0c9165b2966ffd1e20832330c
Derek Zhang <chromestone@users.noreply.github.com>
2022-12-03 23:36:31 -0500

    stegnet batch normalization

--- stegnet/model.py
+++ stegnet/model.py
@@ -32,19 +32,22 @@ class Spatial2Channel(nn.Module):
 
 		super().__init__()
 
-		self.activation = activation
 		assert in_channels <= out_channels
 		# add padding for extra channels produced and no padding for height and width
 		# basically the first in_channels number of channels
 		# in the output will have skip connections
 		# the padding is applied backwards and I don't know why
 		self.padding = (0, 0, 0, 0, 0, out_channels - in_channels)
+
+		self.bnorm = nn.BatchNorm2d(in_channels)
+		self.activation = activation
 		self.sep_conv = SeparableConv2d(in_channels, out_channels, kernel_size, padding='same')
 
 	def forward(self, x):
 
 		padded_x = F.pad(x, self.padding, 'constant', 0)
 
+		x = self.bnorm(x)
 		x = self.activation(x)
 		x = self.sep_conv(x)
 		# skip connection
@@ -64,9 +67,11 @@ class Stegnet(nn.Module):
 		self.layer3 = Spatial2Channel(nn.ELU(), 64, 64, 3)
 		self.layer4 = Spatial2Channel(nn.ELU(), 64, 128, 3)
 		self.layer5 = Spatial2Channel(nn.ELU(), 128, 128, 3)
+		self.bnorm5 = nn.BatchNorm2d(128)
 		self.activ5 = nn.ELU()
 
 		self.layer6 = nn.Conv2d(128, 32, 3, padding='same')
+		self.bnorm6 = nn.BatchNorm2d(32)
 		self.activ6 = nn.ELU()
 		self.layer7 = nn.Conv2d(32, 3, 3, padding='same')
 
@@ -78,9 +83,11 @@ class Stegnet(nn.Module):
 		x = self.layer3(x)
 		x = self.layer4(x)
 		x = self.layer5(x)
+		x = self.bnorm5(x)
 		x = self.activ5(x)
 
 		x = self.layer6(x)
+		x = self.bnorm6(x)
 		x = self.activ6(x)
 		x = self.layer7(x)
 
