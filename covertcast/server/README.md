33ca4babc24b76fbe3ef16fe49516a005573b29b
Derek Zhang <chromestone@users.noreply.github.com>
2022-12-18 13:20:11 -0500

    broadcast multiple images script

--- covertcast/server/README.md
+++ covertcast/server/README.md
@@ -3,3 +3,5 @@
 This code should encode messages and deliver them as images to a YouTube live stream (preferrable in a loop.)
 
 Make sure you write the image in a temporary spot and only mv them to out.jpg when ready.
+
+Scripts derived from https://github.com/rfmcpherson/CovertCast .
--- covertcast/server/broadcast-multiple-yt.sh
+++ covertcast/server/broadcast-multiple-yt.sh
@@ -3,19 +3,13 @@
 # Source:
 # https://github.com/rfmcpherson/CovertCast/blob/master/covertcast/broadcast-yt.sh
 
-# check out https://trac.ffmpeg.org/wiki/Encode/H.264
-
-# -g 60 ???
-#ffmpeg -re -loop 1 -i out.jpg -re -f lavfi -i aevalsrc=0 -codec:v libx264 -video_size 1280x720 -pix_fmt yuv420p -profile:v main -minrate 3000k -maxrate 3000k -b:v 3000k -c:a libvo_aacenc -g 30 -f flv -strict experimental rtmp://a.rtmp.youtube.com/live2/XXX
-
-
 INPUT=""
 # Read input as native framerate
 INPUT="$INPUT -re"
 # Loop over input stream
 INPUT="$INPUT -loop 1"
 # Input file
-INPUT="$INPUT -i out.jpg"
+INPUT="$INPUT -i %03d.png"
 
 AUDIO=""
 # Read input as native framerate
@@ -37,11 +31,10 @@ OUTPUT="$OUTPUT -video_size 1280x720"
 # Attempt to force bitrate at 3000k
 OUTPUT="$OUTPUT -b:v 1500k -maxrate 1500k -minrate 1500k"
 # frames per second
-OUTPUT="$OUTPUT -r 25"
+OUTPUT="$OUTPUT -framerate 1"
 # quality
 OUTPUT="$OUTPUT -q:v 20"
 # Audio codec
-#OUTPUT="$OUTPUT -c:a libvo_aacenc"
 OUTPUT="$OUTPUT -c:a aac"
 # GOP size (calculates intraframes)
 OUTPUT="$OUTPUT -g 30"
@@ -51,7 +44,6 @@ OUTPUT="$OUTPUT -f flv"
 OUTPUT="$OUTPUT -strict experimental"
 # Testing
 OUTPUT="$OUTPUT rtmp://a.rtmp.youtube.com/live2/DO_NOT_LEAK_YOUR_KEY"
-#OUTPUT="$OUTPUT whatever3.flv"
 
 ffmpeg $INPUT $AUDIO -threads 8 -y $OUTPUT
 
